f8d561aeb95927c4321c9d8f2eae6975e213b2b8
Martin Bonnin <martin@mbonnin.net>
2019-10-01 00:33:55 +0200

    remove unused code

--- app/src/main/java/net/mbonnin/messages/mapper/MapperExtensions.kt
+++
@@ -1,6 +0,0 @@
-package net.mbonnin.messages.mapper
-
-import net.mbonnin.messages.database.Dbmessage
-import net.mbonnin.messages.jsonmodel.Message
-
-fun Dbmessage.toMessage() = Message(id = id, content = content, userId = userId, attachments = null)
